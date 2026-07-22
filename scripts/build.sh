578af38a0e60a9e6cbbfb236a81c24a2d286e801
Smathi Lagui <23463016+SmathiLagui@users.noreply.github.com>
2026-07-22 15:50:26 +0200

    switch from mingw to ucrt

--- scripts/build.sh
+++ scripts/build.sh
@@ -56,24 +56,24 @@ echo ""
 # --- Prerequisites ---
 declare -A MISSING_PKGS  # maps label -> "apt:X dnf:X pacman:X msys2:X brew:X"
 check_cmd git                    || MISSING_PKGS[git]="apt:git dnf:git pacman:git msys2:git brew:git"
-check_cmd pkg-config             || MISSING_PKGS[pkg-config]="apt:pkg-config dnf:pkgconf-pkg-config pacman:pkgconf msys2:mingw-w64-x86_64-pkgconf brew:pkg-config"
-check_cmd python3                || MISSING_PKGS[python3]="apt:python3 dnf:python3 pacman:python msys2:mingw-w64-x86_64-python brew:python"
-check_cmd cmake                  || MISSING_PKGS[cmake]="apt:cmake dnf:cmake pacman:cmake msys2:mingw-w64-x86_64-cmake brew:cmake"
-check_cmd ninja                  || MISSING_PKGS[ninja]="apt:ninja-build dnf:ninja-build pacman:ninja msys2:mingw-w64-x86_64-ninja brew:ninja"
-check_cmd gcc                    || MISSING_PKGS[gcc]="apt:gcc dnf:gcc pacman:gcc msys2:mingw-w64-x86_64-gcc brew:gcc"
-check_cmd g++                    || MISSING_PKGS[g++]="apt:g++ dnf:gcc-c++ pacman:gcc msys2:mingw-w64-x86_64-gcc brew:gcc"
-check_cmd arm-none-eabi-gcc      || MISSING_PKGS[arm-none-eabi-gcc]="apt:gcc-arm-none-eabi dnf:arm-none-eabi-gcc-cs pacman:arm-none-eabi-gcc msys2:mingw-w64-x86_64-arm-none-eabi-gcc brew:arm-none-eabi-gcc"
+check_cmd pkg-config             || MISSING_PKGS[pkg-config]="apt:pkg-config dnf:pkgconf-pkg-config pacman:pkgconf msys2:mingw-w64-ucrt-x86_64-pkgconf brew:pkg-config"
+check_cmd python3                || MISSING_PKGS[python3]="apt:python3 dnf:python3 pacman:python msys2:mingw-w64-ucrt-x86_64-python brew:python"
+check_cmd cmake                  || MISSING_PKGS[cmake]="apt:cmake dnf:cmake pacman:cmake msys2:mingw-w64-ucrt-x86_64-cmake brew:cmake"
+check_cmd ninja                  || MISSING_PKGS[ninja]="apt:ninja-build dnf:ninja-build pacman:ninja msys2:mingw-w64-ucrt-x86_64-ninja brew:ninja"
+check_cmd gcc                    || MISSING_PKGS[gcc]="apt:gcc dnf:gcc pacman:gcc msys2:mingw-w64-ucrt-x86_64-gcc brew:gcc"
+check_cmd g++                    || MISSING_PKGS[g++]="apt:g++ dnf:gcc-c++ pacman:gcc msys2:mingw-w64-ucrt-x86_64-gcc brew:gcc"
+check_cmd arm-none-eabi-gcc      || MISSING_PKGS[arm-none-eabi-gcc]="apt:gcc-arm-none-eabi dnf:arm-none-eabi-gcc-cs pacman:arm-none-eabi-gcc msys2:mingw-w64-ucrt-x86_64-arm-none-eabi-gcc brew:arm-none-eabi-gcc"
 check_cmd arm-none-eabi-gcc && {
   echo "" | arm-none-eabi-gcc -x c -include ctype.h -fsyntax-only - &>/dev/null || \
-    MISSING_PKGS[libnewlib]="apt:libnewlib-arm-none-eabi dnf:arm-none-eabi-newlib pacman:arm-none-eabi-newlib msys2:mingw-w64-x86_64-arm-none-eabi-newlib"
+    MISSING_PKGS[libnewlib]="apt:libnewlib-arm-none-eabi dnf:arm-none-eabi-newlib pacman:arm-none-eabi-newlib msys2:mingw-w64-ucrt-x86_64-arm-none-eabi-newlib"
   echo "" | arm-none-eabi-g++ -x c++ -include cstdlib -fsyntax-only - &>/dev/null || \
-    MISSING_PKGS[libstdc++-arm]="apt:libstdc++-arm-none-eabi-newlib dnf:arm-none-eabi-gcc-cs-c++ pacman:arm-none-eabi-newlib msys2:mingw-w64-x86_64-arm-none-eabi-newlib"
+    MISSING_PKGS[libstdc++-arm]="apt:libstdc++-arm-none-eabi-newlib dnf:arm-none-eabi-gcc-cs-c++ pacman:arm-none-eabi-newlib msys2:mingw-w64-ucrt-x86_64-arm-none-eabi-newlib"
 } || {
-  MISSING_PKGS[libnewlib]="apt:libnewlib-arm-none-eabi dnf:arm-none-eabi-newlib pacman:arm-none-eabi-newlib msys2:mingw-w64-x86_64-arm-none-eabi-newlib"
+  MISSING_PKGS[libnewlib]="apt:libnewlib-arm-none-eabi dnf:arm-none-eabi-newlib pacman:arm-none-eabi-newlib msys2:mingw-w64-ucrt-x86_64-arm-none-eabi-newlib"
   MISSING_PKGS[libstdc++-arm]="apt:libstdc++-arm-none-eabi-newlib dnf:arm-none-eabi-gcc-cs-c++ pacman:arm-none-eabi-newlib"
 }
-check_pkg_config libusb-1.0      || MISSING_PKGS[libusb]="apt:libusb-1.0-0-dev dnf:libusb-devel pacman:libusb msys2:mingw-w64-x86_64-libusb brew:libusb"
-check_python_module Cryptodome   || MISSING_PKGS[pycryptodomex]="apt:python3-pycryptodome dnf:python3-pycryptodomex pacman:python-pycryptodomex msys2:mingw-w64-x86_64-python-pycryptodomex brew:\"pip3 install pycryptodomex\""
+check_pkg_config libusb-1.0      || MISSING_PKGS[libusb]="apt:libusb-1.0-0-dev dnf:libusb-devel pacman:libusb msys2:mingw-w64-ucrt-x86_64-libusb brew:libusb"
+check_python_module Cryptodome   || MISSING_PKGS[pycryptodomex]="apt:python3-pycryptodome dnf:python3-pycryptodomex pacman:python-pycryptodomex msys2:mingw-w64-ucrt-x86_64-python-pycryptodomex brew:\"pip3 install pycryptodomex\""
 
 if [ ${#MISSING_PKGS[@]} -gt 0 ]; then
   echo "Missing required tools: ${!MISSING_PKGS[*]}"
@@ -93,8 +93,8 @@ if [ ${#MISSING_PKGS[@]} -gt 0 ]; then
   }
 
   if [ -n "$MSYSTEM" ] && check_cmd pacman; then
-    echo "  MSYS2 MinGW64:  pacman -S $(get_pkgs msys2)"
-    echo "  Make sure you are running this script in the MSYS2 MinGW64 shell."
+    echo "  MSYS2 UCRT64:   pacman -S $(get_pkgs msys2)"
+    echo "  Make sure you are running this script in the MSYS2 UCRT64 shell."
   elif check_cmd apt-get; then
     echo "  Debian/Ubuntu:  sudo apt install $(get_pkgs apt)"
   elif check_cmd dnf; then
